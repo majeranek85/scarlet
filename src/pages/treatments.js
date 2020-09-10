2139bf0b76e81480d430655c7b340f4aaaef2482
Marta Majerowska <majeranek85@gmail.com>
2020-09-10 17:11:06 +0200

    Initial commit

---
+++ src/pages/treatments.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Treatments = () => {
+  return (
+    <Layout>
+      <SEO title='Zabiegi' />
+      <h1>Zabiegi</h1>
+    </Layout>
+  )
+}
+
+export default Treatments
---
+++ src/utils/theme.js
@@ -0,0 +1,5 @@
+export const theme = {
+  primary: '#fedbd0',
+  secondary: '#feeae6',
+  text: '#442c2e'
+}
